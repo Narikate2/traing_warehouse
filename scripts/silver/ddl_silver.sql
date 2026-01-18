d2c29eada9ecc208296d2d6771aed021095fde20
Evgeny <rudy_excelent@mail.ru>
2026-01-18 16:48:29 +0300

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -7,8 +7,9 @@ DDL script: Создание таблиц для бронзового слоя
   В случае если таблица с таким названием уже существует, она удалется и создается повторно
 
 Замечание:
-  Код создания таблиц идентичен бронзовому слою, за исключением добавленного столбца для метаданных: 
-  dwh_create_date - атрибут, отображающий время добавления данных
+  Код создания таблиц идентичен бронзовому слою, за исключением
+  добавленного столбца для метаданных и измененных типов данных для некоторых атрибутов: 
+  
 ===================================
 
 */
@@ -42,23 +43,23 @@ create table silver.crm_prd_info (
 	prd_start_dt	date,
 	prd_end_dt		date,
 dwh_create_date		datetime2 default getdate()
-	);
+);
 go
 
 drop table if exists silver.crm_sales_details;
 go
 
 create table silver.crm_sales_details (
-sls_ord_num		nvarchar(50),
-sls_prd_key		nvarchar(50),
-sls_cust_id		int,
-sls_order_dt	int,
-sls_ship_dt		int,
-sls_due_dt		int,
-sls_sales		int,
-sls_quantity	int,
-sls_price		int,
-dwh_create_date		datetime2 default getdate()
+	sls_ord_num		nvarchar(50),
+	sls_prd_key		nvarchar(50),
+	sls_cust_id		int,
+	sls_order_dt	date,
+	sls_ship_dt		date,
+	sls_due_dt		date,
+	sls_sales		int,
+	sls_quantity	int,
+	sls_price		int,
+	dwh_create_date	datetime2 default getdate()
 );
 go
 
@@ -66,10 +67,10 @@ drop table if exists silver.erp_cust_az12;
 go
 
 create table silver.erp_cust_az12 (
-cid		nvarchar(50),
-bdate	date,
-gen		nvarchar(50),
-dwh_create_date		datetime2 default getdate()
+	cid					nvarchar(50),
+	bdate				date,
+	gen					nvarchar(50),
+	dwh_create_date		datetime2 default getdate()
 );
 go
 
@@ -77,12 +78,11 @@ drop table if exists silver.erp_loc_a101;
 go
 
 create table silver.erp_loc_a101 (
-	cid		nvarchar(50),
-	cntry	nvarchar(50),
+	cid					nvarchar(50),
+	cntry				nvarchar(50),
 	dwh_create_date		datetime2 default getdate()
 );
 go
-
 
 drop table if exists silver.erp_px_cat_g1v2;
 go
@@ -92,7 +92,7 @@ create table silver.erp_px_cat_g1v2 (
 	cat				nvarchar(50),
 	subcat			nvarchar(50),
 	maintenance		nvarchar(50),
-	dwh_create_date		datetime2 default getdate()
+	dwh_create_date	datetime2 default getdate()
 );
 go
 
